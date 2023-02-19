de3bdf6f41f82bad6b7c0ba9ce792f1c2682c925
Aniket Kotal <aniket1010104@gmail.com>
2023-02-19 23:10:54 +0530

    Fixed Typing bug

--- src/components/ui/Icon.vue
+++ src/components/ui/Icon.vue
@@ -7,15 +7,14 @@
     class="flex justify-center disabled:opacity-40 transition-all
     items-center aspect-square w-8 h-full hover:brightness-125"
   >
-    <Icon :class="{
+    <Iconify :class="{
       'text-header-secondary': !disabled,
-    }" :icon="icon"
-          class="transition-all w-full text-3xl" v-bind="$attrs" />
+    }" class="transition-all w-full text-3xl" v-bind="$attrs" />
   </button>
 </template>
 
 <script lang="ts" setup>
-import { Icon } from "@iconify/vue";
+import { Icon as Iconify } from "@iconify/vue";
 
 defineProps<{
   disabled?: boolean;
